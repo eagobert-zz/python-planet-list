317078e9a0c87c828e143792bb6e029ad32f01d5
eagobert <eagobert@hotmail.com>
2018-07-05 11:48:22 -0500

    add questions for practicing tuples. still working

--- planets.py
+++ planets.py
@@ -33,3 +33,8 @@ print(rocky_planets)
 del planet_list[8]
 
 print(planet_list)
+
+
+# Create another list containing tuples. Each tuple will hold the name of a spacecraft that we have launched, and the names of the planet(s) that it has visited, or landed on. (e.g. ('Cassini', 'Saturn')).
+
+# Iterate over your list of planets, and inside that loop, iterate over the list of tuples. Print, for each planet, which sattelites have visited.
