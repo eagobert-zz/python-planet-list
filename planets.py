fff23f32099d26c92dce416d2ebe3c37e5948b6f
eagobert <eagobert@hotmail.com>
2018-07-13 22:10:59 -0500

    working last question

--- planets.py
+++ planets.py
@@ -37,4 +37,14 @@ print(planet_list)
 
 # Create another list containing tuples. Each tuple will hold the name of a spacecraft that we have launched, and the names of the planet(s) that it has visited, or landed on. (e.g. ('Cassini', 'Saturn')).
 
-# Iterate over your list of planets, and inside that loop, iterate over the list of tuples. Print, for each planet, which sattelites have visited.
+spacecraft = [('Cassini', 'Saturn'), ('Mariner', 'Venus', 'Mars', 'Mercury'), ('Viking 1', 'Mars')]
+
+print(spacecraft)
+
+# Iterate over your list of planets, and inside that loop, iterate over the list of tuples. Print, for each planet, which satellites have visited.
+
+for planet in planet_list:
+    # print(planet)
+    for craft in spacecraft:
+        if planet == :
+            print(craft)
